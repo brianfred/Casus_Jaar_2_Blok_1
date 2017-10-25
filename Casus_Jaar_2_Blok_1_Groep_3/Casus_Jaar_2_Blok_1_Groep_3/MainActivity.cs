76c196670a988a134d4660738e1992dd83bcdd4b
Brian Dols <1639013dols@zuyd.nl>
2017-10-25 14:14:52 +0200

    Start leeg project

---
+++ Casus_Jaar_2_Blok_1_Groep_3/Casus_Jaar_2_Blok_1_Groep_3/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Casus_Jaar_2_Blok_1_Groep_3
+{
+    [Activity(Label = "Casus_Jaar_2_Blok_1_Groep_3", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
