f1b48268fc4819345bcb72711616ac5df522f61d
Brian Dols <1639013dols@zuyd.nl>
2017-10-27 11:35:53 +0200

    Verschillende layouts aangemaakt en aangeroepen (nog niet grapisch aangemaakt)

--- Casus_Jaar_2_Blok_1_Groep_3/Casus_Jaar_2_Blok_1_Groep_3/MainActivity.cs
+++ Casus_Jaar_2_Blok_1_Groep_3/Casus_Jaar_2_Blok_1_Groep_3/MainActivity.cs
@@ -12,7 +12,44 @@ namespace Casus_Jaar_2_Blok_1_Groep_3
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.Main);
+            SetContentView(Resource.Layout.LayoutMenu);
+
+            btnMenuNetflix = FindViewById<Button>(Resource.Id.btnNetflix);
+            btnMenuNetflix.Click += delegate
+            {
+                openNetflixMenu();   
+            };
+
+            btnMenuYoutube = FindViewById<Button>(Resource.Id.btnYoutube);
+            btnMenuYoutube.Click += delegate
+            {
+                openYoutubeMenu();
+            };
+
+            btnMenuZiggo = FindViewById<Button>(Resource.Id.btnNetflix);
+            btnMenuZiggo.Click += delegate
+            {
+                openZiggoMenu();
+            };
+
+            btnMenuInstellingen = FindViewById<Button>(Resource.Id.btnInstellingen);
+            {
+
+            };
+        }
+        private void openNetFlixMenu()
+        {
+            SetContentView(Resource.Layout.LayoutNetflixMenu);
+        }
+
+        private void openYoutubeMenu()
+        {
+            SetContentView(Resource.Layout.LayoutYoutubeMenu);
+        }
+
+        private void openZiggoMenu()
+        {
+            SetContentView(Resource.Layout.LayoutZiggoMenu);
         }
     }
 }
