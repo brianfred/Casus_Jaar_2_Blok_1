1fb21daeca687c214d21118a713726410a452215
Bolk, Luc (1612255) <1612255bolk@zuyd.nl>
2017-11-06 12:13:39 +0100

    Supah

--- Casus_Jaar_2_Blok_1_Groep_3/Casus_Jaar_2_Blok_1_Groep_3/Resources/Resource.Designer.cs
+++ Casus_Jaar_2_Blok_1_Groep_3/Casus_Jaar_2_Blok_1_Groep_3/Resources/Resource.Designer.cs
@@ -44,59 +44,59 @@ namespace Casus_Jaar_2_Blok_1_Groep_3
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f04000c
-			public const int btnAccountToevoegen = 2130968588;
+			// aapt resource value: 0x7f04000f
+			public const int btnAccountToevoegen = 2130968591;
 			
-			// aapt resource value: 0x7f04000d
-			public const int btnAccountVerwijderen = 2130968589;
+			// aapt resource value: 0x7f040010
+			public const int btnAccountVerwijderen = 2130968592;
 			
-			// aapt resource value: 0x7f04000b
-			public const int btnFavorieten = 2130968587;
+			// aapt resource value: 0x7f04000e
+			public const int btnFavorieten = 2130968590;
 			
 			// aapt resource value: 0x7f040003
 			public const int btnInloggen = 2130968579;
 			
-			// aapt resource value: 0x7f040012
-			public const int btnInstellingen = 2130968594;
+			// aapt resource value: 0x7f040015
+			public const int btnInstellingen = 2130968597;
 			
 			// aapt resource value: 0x7f040000
 			public const int btnInstellingenAccountToevoegen = 2130968576;
 			
-			// aapt resource value: 0x7f040009
-			public const int btnInstellingenBlok = 2130968585;
+			// aapt resource value: 0x7f04000c
+			public const int btnInstellingenBlok = 2130968588;
 			
-			// aapt resource value: 0x7f04000a
-			public const int btnInstellingenTerug = 2130968586;
+			// aapt resource value: 0x7f04000d
+			public const int btnInstellingenTerug = 2130968589;
 			
 			// aapt resource value: 0x7f040002
 			public const int btnLogoAccount = 2130968578;
 			
-			// aapt resource value: 0x7f040013
-			public const int btnNetflix = 2130968595;
+			// aapt resource value: 0x7f040016
+			public const int btnNetflix = 2130968598;
 			
-			// aapt resource value: 0x7f040006
-			public const int btnNetflixInstellingen = 2130968582;
+			// aapt resource value: 0x7f040009
+			public const int btnNetflixInstellingen = 2130968585;
 			
-			// aapt resource value: 0x7f04000e
-			public const int btnTaalVeranderen = 2130968590;
+			// aapt resource value: 0x7f040011
+			public const int btnTaalVeranderen = 2130968593;
 			
 			// aapt resource value: 0x7f040001
 			public const int btnTerugNaarMenu = 2130968577;
 			
-			// aapt resource value: 0x7f040010
-			public const int btnTerugOpenProgram = 2130968592;
+			// aapt resource value: 0x7f040013
+			public const int btnTerugOpenProgram = 2130968595;
 			
-			// aapt resource value: 0x7f040014
-			public const int btnYoutube = 2130968596;
+			// aapt resource value: 0x7f040017
+			public const int btnYoutube = 2130968599;
 			
-			// aapt resource value: 0x7f040007
-			public const int btnYoutubeInstellingen = 2130968583;
+			// aapt resource value: 0x7f04000a
+			public const int btnYoutubeInstellingen = 2130968586;
 			
-			// aapt resource value: 0x7f040015
-			public const int btnZiggo = 2130968597;
+			// aapt resource value: 0x7f040018
+			public const int btnZiggo = 2130968600;
 			
-			// aapt resource value: 0x7f040008
-			public const int btnZiggoInstellingen = 2130968584;
+			// aapt resource value: 0x7f04000b
+			public const int btnZiggoInstellingen = 2130968587;
 			
 			// aapt resource value: 0x7f040005
 			public const int etPassword = 2130968581;
@@ -104,11 +104,20 @@ namespace Casus_Jaar_2_Blok_1_Groep_3
 			// aapt resource value: 0x7f040004
 			public const int etUsername = 2130968580;
 			
-			// aapt resource value: 0x7f040011
-			public const int linearLayout1 = 2130968593;
+			// aapt resource value: 0x7f040014
+			public const int linearLayout1 = 2130968596;
 			
-			// aapt resource value: 0x7f04000f
-			public const int txtOpenProgram = 2130968591;
+			// aapt resource value: 0x7f040006
+			public const int txtNetflixACCV = 2130968582;
+			
+			// aapt resource value: 0x7f040012
+			public const int txtOpenProgram = 2130968594;
+			
+			// aapt resource value: 0x7f040007
+			public const int txtYoutubeACCV = 2130968583;
+			
+			// aapt resource value: 0x7f040008
+			public const int txtZiggoACCV = 2130968584;
 			
 			static Id()
 			{
