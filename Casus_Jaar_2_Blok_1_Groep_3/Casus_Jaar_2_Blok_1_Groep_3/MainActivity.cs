b701c3ae40809b14df5ce361569f049c3c7f6edf
Brian Dols <1639013dols@zuyd.nl>
2017-11-07 18:08:20 +0100

    Veel functioneel gemaakt

--- Casus_Jaar_2_Blok_1_Groep_3/Casus_Jaar_2_Blok_1_Groep_3/MainActivity.cs
+++ Casus_Jaar_2_Blok_1_Groep_3/Casus_Jaar_2_Blok_1_Groep_3/MainActivity.cs
@@ -16,7 +16,43 @@ namespace Casus_Jaar_2_Blok_1_Groep_3
         private Button btnAccVerwijderen;
         private Button btnTaalVeranderen;
         private Button btnFavorieten;
+        private Button btnMenuFav;
+        private Button btnInstellingenFav;
+        private Button btnMenuAccT;
+        private Button btnInloggen;
+        private Button btnInstellingenAccT;
+        private Button btnNetflixFavorieten1;
+        private Button btnNetflixFavorieten2;
+        private Button btnNetflixFavorieten3;
+        private Button btnNetflixFavorieten4;
+        private Button btnYoutubeFavorieten1;
+        private Button btnYoutubeFavorieten2;
+        private Button btnYoutubeFavorieten3;
+        private Button btnYoutubeFavorieten4;
+        private Button btnZiggoFavorieten1;
+        private Button btnZiggoFavorieten2;
+        private Button btnZiggoFavorieten3;
+        private Button btnZiggoFavorieten4;
+        private Button btnLogoAccount;
+        private Button btnMenuAccV;
+        private Button btnInstellingenAccV;
+        private Button btnNetflixAccV;
+        private Button btnYoutubeAccV;
+        private Button btnZiggoAccV;
+        private TextView txtNetflixAcc;
+        private TextView txtYoutubeAcc;
+        private TextView txtZiggoAcc;
+        private TextView txtOpenProgram;
+        private EditText etUsername;
+        private EditText etPassword;
         private string huidigeTaal;
+        private string NetflixUsername;
+        private string NetflixPassword;
+        private string YoutubeUsername;
+        private string YoutubePassword;
+        private string ZiggoUsername;
+        private string ZiggoPassword;
+        private string LogoAccountMaken;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -24,94 +60,362 @@ namespace Casus_Jaar_2_Blok_1_Groep_3
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.main); //LayoutMenu);
             huidigeTaal = "Nederlands";
+            NetflixUsername = "Geen Account";
+            YoutubeUsername = "Geen Account";
+            ZiggoUsername = "Geen Account";
+            NetflixPassword = "";
+            ZiggoPassword = "";
+            YoutubePassword = "";
+            LogoAccountMaken = "Netflix";
             OpenMenu();
         }
 
         private void OpenMenu()
         {
+            SetContentView(Resource.Layout.main);
+            string program = "";
+            string komtVanProgram = "Menu";
             btnMenuNetflix = FindViewById<Button>(Resource.Id.btnNetflix);
             btnMenuNetflix.Click += delegate
             {
-                string program = "Netflix";
-                OpenProgram(program);
+                program = "Netflix";
+                OpenProgram(program, komtVanProgram);
             };
 
             btnMenuYoutube = FindViewById<Button>(Resource.Id.btnYoutube);
             btnMenuYoutube.Click += delegate
             {
-                string program = "Youtube";
-                OpenProgram(program);
+                program = "Youtube";
+                OpenProgram(program, komtVanProgram);
             };
 
             btnMenuZiggo = FindViewById<Button>(Resource.Id.btnZiggo);
             btnMenuZiggo.Click += delegate
             {
-                string program = "Ziggo";
-                OpenProgram(program);
+                program = "Ziggo";
+                OpenProgram(program, komtVanProgram);
             };
 
             btnMenuInstellingen = FindViewById<Button>(Resource.Id.btnInstellingen);
             btnMenuInstellingen.Click += delegate
             {
-                OpenInstellingen();
+                string PaginaVandaan = "Menu";
+                OpenInstellingen(PaginaVandaan);
             };
         }
 
-        private void OpenProgram(string program)
+        private void OpenProgram(string program, string komtVanProgram)
         {
             SetContentView(Resource.Layout.LayoutOpenProgram);
+            TextView txtOpenProgram = FindViewById<TextView>(Resource.Id.txtOpenProgram);
+            txtOpenProgram.Text = string.Format("U gaat nu naar {0}", program);
             Button btnTerugOP = FindViewById<Button>(Resource.Id.btnTerugOpenProgram);
             btnTerugOP.Click += delegate
             {
-                SetContentView(Resource.Layout.main);
-                OpenMenu();
+                if (komtVanProgram == "Menu")
+                {
+                    OpenMenu();
+                }
+                if (komtVanProgram == "Favorieten")
+                {
+                    Favorieten();
+                }
             };
         }
         
-        private void OpenInstellingen()
+        private void OpenInstellingen(string PaginaVandaan)
         {
             SetContentView(Resource.Layout.LayoutInstellingen);
             btnTerugInst = FindViewById<Button>(Resource.Id.btnInstellingenTerug);
             btnTerugInst.Click += delegate
             {
-                SetContentView(Resource.Layout.main);
-                OpenMenu();
+                if (PaginaVandaan == "Menu")
+                {
+                    OpenMenu();
+                }
+
+                if (PaginaVandaan == "Fav")
+                {
+                    Favorieten();
+                }
+
+                if (PaginaVandaan == "AccT")
+                {
+                    AccToevoegen();
+                }
+
+                if (PaginaVandaan == "AccV")
+                {
+                    AccVerwijderen();
+                }
+                    
             };
 
             btnAccToevoegen = FindViewById<Button>(Resource.Id.btnAccountToevoegen);
             btnAccToevoegen.Click += delegate
             {
-                SetContentView(Resource.Layout.LayoutAccountToevoegen);
-                
+                AccToevoegen();
             };
 
             btnAccVerwijderen = FindViewById<Button>(Resource.Id.btnAccountVerwijderen);
             btnAccVerwijderen.Click += delegate
             {
-                SetContentView(Resource.Layout.LayoutAccountVerwijderen);
+                AccVerwijderen();
             };
-
             btnTaalVeranderen = FindViewById<Button>(Resource.Id.btnTaalVeranderen);
+            if (huidigeTaal == "Nederlands")
+            {
+                btnTaalVeranderen.Text = string.Format("Taal: {0}", huidigeTaal);
+            }
+            
+            if (huidigeTaal == "English")
+            {
+                btnTaalVeranderen.Text = string.Format("Language: {0}", huidigeTaal);
+            }
             btnTaalVeranderen.Click += delegate
             {
                 if (huidigeTaal == "Nederlands")
                 {
-                    huidigeTaal = "Engels";
-                    btnTaalVeranderen.Text = "Language: English";
+                    huidigeTaal = "English";
+                    btnTaalVeranderen.Text = string.Format("Language: {0}", huidigeTaal);
                 }
                 else
                 {
-                    if (huidigeTaal == "Engels")
+                    if (huidigeTaal == "English")
                     {
                         huidigeTaal = "Nederlands";
-                        btnTaalVeranderen.Text = "Taal: Nederlands";
+                        btnTaalVeranderen.Text = string.Format("Taal: {0}", huidigeTaal);
                     }
                 }
+                
             };
             btnFavorieten = FindViewById <Button>(Resource.Id.btnFavorieten);
             btnFavorieten.Click += delegate
             {
+                Favorieten();
+            };
 
+        }
+        private void AccToevoegen()
+        {
+            SetContentView(Resource.Layout.LayoutAccountToevoegen);
+            btnInstellingenAccT = FindViewById<Button>(Resource.Id.btnInstellingenAccT);
+            btnMenuAccT = FindViewById<Button>(Resource.Id.btnTerugNaarMenuAccT);
+            btnLogoAccount = FindViewById<Button>(Resource.Id.btnLogoAccount);
+            etUsername = FindViewById<EditText>(Resource.Id.etUsername);
+            etPassword = FindViewById<EditText>(Resource.Id.etPassword);
+            btnInloggen = FindViewById<Button>(Resource.Id.btnInloggen);
+            etUsername.Text = "Username";
+            etPassword.Text = "Password";
+            btnLogoAccount.Text = string.Format("{0}", LogoAccountMaken);
+            btnInstellingenAccT.Click += delegate
+            {
+                string PaginaVandaan = "AccT";
+                OpenInstellingen(PaginaVandaan);
+            };
+
+            btnMenuAccT.Click += delegate
+            {
+                OpenMenu();
+            };
+
+            btnLogoAccount.Click += delegate
+            {
+                if (LogoAccountMaken == "Netflix")
+                {
+                    LogoAccountMaken = "Youtube";
+                }
+                else
+                {
+                    if (LogoAccountMaken == "Youtube")
+                    {
+                        LogoAccountMaken = "Ziggo";
+                    }
+                    else
+                    {
+                        if (LogoAccountMaken == "Ziggo")
+                        {
+                            LogoAccountMaken = "Netflix";
+                        }
+                    }
+                }
+                btnLogoAccount.Text = string.Format("{0}", LogoAccountMaken);
+                
+            };
+
+            btnInloggen.Click += delegate
+            {
+                if (etUsername.Text != "" && etUsername.Text != "Username" && etPassword.Text != "" && etPassword.Text != "Password")
+                {
+                        if (LogoAccountMaken == "Youtube")
+                        {
+                            YoutubeUsername = etUsername.Text;
+                            YoutubePassword = etPassword.Text;
+                        }
+
+                        if (LogoAccountMaken == "Netflix")
+                        {
+                            NetflixUsername = etUsername.Text;
+                            NetflixPassword = etPassword.Text;
+                        }
+
+                        if (LogoAccountMaken == "Ziggo")
+                        {
+                            ZiggoUsername = etUsername.Text;
+                            ZiggoPassword = etPassword.Text;
+                        
+                        }
+                }
+                AccToevoegen();
+            };
+
+            
+
+
+
+
+
+        }
+
+        private void AccVerwijderen()
+        {
+            SetContentView(Resource.Layout.LayoutAccountVerwijderen);
+            btnInstellingenAccV = FindViewById<Button>(Resource.Id.btnInstellingenAccV);
+            btnMenuAccV = FindViewById<Button>(Resource.Id.btnMenuAccV);
+            btnNetflixAccV = FindViewById<Button>(Resource.Id.btnNetflixAccountVerwijderen);
+            btnYoutubeAccV = FindViewById<Button>(Resource.Id.btnYoutubeAccountVerwijderen);
+            btnZiggoAccV = FindViewById<Button>(Resource.Id.btnZiggoAccountVerwijderen);
+            txtNetflixAcc = FindViewById<TextView>(Resource.Id.txtNetflixAccountVerwijderen);
+            txtYoutubeAcc = FindViewById<TextView>(Resource.Id.txtYoutubeAccountVerwijderen);
+            txtZiggoAcc = FindViewById<TextView>(Resource.Id.txtZiggoAccountVerwijderen);
+            txtNetflixAcc.Text = string.Format("{0}", NetflixUsername);
+            txtZiggoAcc.Text = string.Format("{0}", ZiggoUsername);
+            txtYoutubeAcc.Text = string.Format("{0}", YoutubeUsername);
+
+            btnInstellingenAccV.Click += delegate
+            {
+                string PaginaVandaan = "AccV";
+                OpenInstellingen(PaginaVandaan);
+            };
+
+            btnMenuAccV.Click += delegate
+            {
+                OpenMenu();
+            };
+
+            btnNetflixAccV.Click += delegate
+            {
+                NetflixUsername = "Geen Account";
+                NetflixPassword = "";
+                txtNetflixAcc.Text = string.Format("{0}", NetflixUsername);
+            };
+
+            btnYoutubeAccV.Click += delegate
+            {
+                YoutubeUsername = "Geen Account";
+                YoutubePassword = "";
+                txtYoutubeAcc.Text = string.Format("{0}", YoutubeUsername);
+            };
+
+            btnZiggoAccV.Click += delegate
+            {
+                ZiggoUsername = "Geen Account";
+                ZiggoPassword = "";
+                txtZiggoAcc.Text = string.Format("{0}", ZiggoUsername);
+            };
+
+        }
+
+        private void Favorieten()
+        {
+            SetContentView(Resource.Layout.LayoutFavorieten);
+            btnMenuFav = FindViewById<Button>(Resource.Id.btnMenuFav);
+            btnMenuFav.Click += delegate
+            {
+                OpenMenu();
+            };
+
+            btnInstellingenFav = FindViewById<Button>(Resource.Id.btnInstellingenFav);
+            btnInstellingenFav.Click += delegate
+            {
+                string paginavandaan = "Fav";
+                OpenInstellingen(paginavandaan);
+            };
+
+            btnNetflixFavorieten1 = FindViewById<Button>(Resource.Id.btnNetflixFavorieten1);
+            btnNetflixFavorieten2 = FindViewById<Button>(Resource.Id.btnNetflixFavorieten2);
+            btnNetflixFavorieten3 = FindViewById<Button>(Resource.Id.btnNetflixFavorieten3);
+            btnNetflixFavorieten4 = FindViewById<Button>(Resource.Id.btnNetflixFavorieten4);
+            btnZiggoFavorieten1 = FindViewById<Button>(Resource.Id.btnZiggoFavorieten1);
+            btnZiggoFavorieten2 = FindViewById<Button>(Resource.Id.btnZiggoFavorieten2);
+            btnZiggoFavorieten3 = FindViewById<Button>(Resource.Id.btnZiggoFavorieten3);
+            btnZiggoFavorieten4 = FindViewById<Button>(Resource.Id.btnZiggoFavorieten4);
+            btnYoutubeFavorieten1 = FindViewById<Button>(Resource.Id.btnYoutubeFavorieten1);
+            btnYoutubeFavorieten2 = FindViewById<Button>(Resource.Id.btnYoutubeFavorieten2);
+            btnYoutubeFavorieten3 = FindViewById<Button>(Resource.Id.btnYoutubeFavorieten3);
+            btnYoutubeFavorieten4 = FindViewById<Button>(Resource.Id.btnYoutubeFavorieten4);
+            string program = "";
+            string komtVanProgram = "Favorieten";
+            btnNetflixFavorieten1.Click += delegate
+            {
+                program = "Netflix Fav 1";
+                OpenProgram(program, komtVanProgram);
+            };
+            btnNetflixFavorieten2.Click += delegate
+            {
+                program = "Netflix Fav 2";
+                OpenProgram(program, komtVanProgram);
+            };
+            btnNetflixFavorieten3.Click += delegate
+            {
+                program = "Netflix Fav 3";
+                OpenProgram(program, komtVanProgram);
+            };
+            btnNetflixFavorieten4.Click += delegate
+            {
+                program = "Netflix Fav 4";
+                OpenProgram(program, komtVanProgram);
+            };
+
+            btnYoutubeFavorieten1.Click += delegate
+            {
+                program = "Youtube Fav 1";
+                OpenProgram(program, komtVanProgram);
+            };
+            btnYoutubeFavorieten2.Click += delegate
+            {
+                program = "Youtube Fav 2";
+                OpenProgram(program, komtVanProgram);
+            };
+            btnYoutubeFavorieten3.Click += delegate
+            {
+                program = "Youtube Fav 3";
+                OpenProgram(program, komtVanProgram);
+            };
+            btnYoutubeFavorieten4.Click += delegate
+            {
+                program = "Youtube Fav 4";
+                OpenProgram(program, komtVanProgram);
+            };
+            btnZiggoFavorieten1.Click += delegate
+            {
+                program = "Ziggo Go Fav 1";
+                OpenProgram(program, komtVanProgram);
+            };
+            btnZiggoFavorieten2.Click += delegate
+            {
+                program = "Ziggo Go Fav 2";
+                OpenProgram(program, komtVanProgram);
+            };
+            btnZiggoFavorieten3.Click += delegate
+            {
+                program = "Ziggo Go Fav 3";
+                OpenProgram(program, komtVanProgram);
+            };
+            btnZiggoFavorieten4.Click += delegate
+            {
+                program = "Ziggo Go Fav 4";
+                OpenProgram(program, komtVanProgram);
             };
         }
     }
--- Casus_Jaar_2_Blok_1_Groep_3/Casus_Jaar_2_Blok_1_Groep_3/Resources/Resource.Designer.cs
+++ Casus_Jaar_2_Blok_1_Groep_3/Casus_Jaar_2_Blok_1_Groep_3/Resources/Resource.Designer.cs
@@ -44,92 +44,119 @@ namespace Casus_Jaar_2_Blok_1_Groep_3
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f040021
-			public const int btnAccountToevoegen = 2130968609;
+			// aapt resource value: 0x7f04002a
+			public const int btnAccountToevoegen = 2130968618;
 			
-			// aapt resource value: 0x7f040022
-			public const int btnAccountVerwijderen = 2130968610;
+			// aapt resource value: 0x7f04002b
+			public const int btnAccountVerwijderen = 2130968619;
 			
-			// aapt resource value: 0x7f040020
-			public const int btnFavorieten = 2130968608;
+			// aapt resource value: 0x7f040029
+			public const int btnFavorieten = 2130968617;
 			
 			// aapt resource value: 0x7f040003
 			public const int btnInloggen = 2130968579;
 			
-			// aapt resource value: 0x7f040027
-			public const int btnInstellingen = 2130968615;
+			// aapt resource value: 0x7f040030
+			public const int btnInstellingen = 2130968624;
 			
 			// aapt resource value: 0x7f040000
-			public const int btnInstellingenAccountToevoegen = 2130968576;
+			public const int btnInstellingenAccT = 2130968576;
 			
-			// aapt resource value: 0x7f04001e
-			public const int btnInstellingenBlok = 2130968606;
+			// aapt resource value: 0x7f040006
+			public const int btnInstellingenAccV = 2130968582;
 			
-			// aapt resource value: 0x7f04001f
-			public const int btnInstellingenTerug = 2130968607;
+			// aapt resource value: 0x7f04000b
+			public const int btnInstellingenAccountToevoegen = 2130968587;
+			
+			// aapt resource value: 0x7f040027
+			public const int btnInstellingenBlok = 2130968615;
+			
+			// aapt resource value: 0x7f040014
+			public const int btnInstellingenFav = 2130968596;
+			
+			// aapt resource value: 0x7f040028
+			public const int btnInstellingenTerug = 2130968616;
 			
 			// aapt resource value: 0x7f040002
 			public const int btnLogoAccount = 2130968578;
 			
-			// aapt resource value: 0x7f040028
-			public const int btnNetflix = 2130968616;
-			
-			// aapt resource value: 0x7f04000c
-			public const int btnNetflixAccountVerwijderen = 2130968588;
-			
-			// aapt resource value: 0x7f040011
-			public const int btnNetflixFavorieten1 = 2130968593;
-			
-			// aapt resource value: 0x7f040012
-			public const int btnNetflixFavorieten2 = 2130968594;
+			// aapt resource value: 0x7f040007
+			public const int btnMenuAccV = 2130968583;
 			
 			// aapt resource value: 0x7f040013
-			public const int btnNetflixFavorieten3 = 2130968595;
+			public const int btnMenuFav = 2130968595;
 			
-			// aapt resource value: 0x7f040014
-			public const int btnNetflixFavorieten4 = 2130968596;
+			// aapt resource value: 0x7f040031
+			public const int btnNetflix = 2130968625;
 			
-			// aapt resource value: 0x7f04001b
-			public const int btnNetflixInstellingen = 2130968603;
-			
-			// aapt resource value: 0x7f040023
-			public const int btnTaalVeranderen = 2130968611;
-			
-			// aapt resource value: 0x7f040001
-			public const int btnTerugNaarMenu = 2130968577;
-			
-			// aapt resource value: 0x7f040025
-			public const int btnTerugOpenProgram = 2130968613;
-			
-			// aapt resource value: 0x7f040029
-			public const int btnYoutube = 2130968617;
-			
-			// aapt resource value: 0x7f04000a
-			public const int btnYoutubeAccountVerwijderen = 2130968586;
+			// aapt resource value: 0x7f04000f
+			public const int btnNetflixAccountVerwijderen = 2130968591;
 			
 			// aapt resource value: 0x7f040016
-			public const int btnYoutubeFavorieten1 = 2130968598;
+			public const int btnNetflixFavorieten1 = 2130968598;
 			
 			// aapt resource value: 0x7f040017
-			public const int btnYoutubeFavorieten2 = 2130968599;
+			public const int btnNetflixFavorieten2 = 2130968599;
 			
 			// aapt resource value: 0x7f040018
-			public const int btnYoutubeFavorieten3 = 2130968600;
+			public const int btnNetflixFavorieten3 = 2130968600;
 			
 			// aapt resource value: 0x7f040019
-			public const int btnYoutubeFavorieten4 = 2130968601;
+			public const int btnNetflixFavorieten4 = 2130968601;
+			
+			// aapt resource value: 0x7f040024
+			public const int btnNetflixInstellingen = 2130968612;
+			
+			// aapt resource value: 0x7f04002c
+			public const int btnTaalVeranderen = 2130968620;
+			
+			// aapt resource value: 0x7f040001
+			public const int btnTerugNaarMenuAccT = 2130968577;
+			
+			// aapt resource value: 0x7f04002e
+			public const int btnTerugOpenProgram = 2130968622;
+			
+			// aapt resource value: 0x7f040032
+			public const int btnYoutube = 2130968626;
+			
+			// aapt resource value: 0x7f04000d
+			public const int btnYoutubeAccountVerwijderen = 2130968589;
+			
+			// aapt resource value: 0x7f04001b
+			public const int btnYoutubeFavorieten1 = 2130968603;
 			
 			// aapt resource value: 0x7f04001c
-			public const int btnYoutubeInstellingen = 2130968604;
-			
-			// aapt resource value: 0x7f04002a
-			public const int btnZiggo = 2130968618;
-			
-			// aapt resource value: 0x7f04000e
-			public const int btnZiggoAccountVerwijderen = 2130968590;
+			public const int btnYoutubeFavorieten2 = 2130968604;
 			
 			// aapt resource value: 0x7f04001d
-			public const int btnZiggoInstellingen = 2130968605;
+			public const int btnYoutubeFavorieten3 = 2130968605;
+			
+			// aapt resource value: 0x7f04001e
+			public const int btnYoutubeFavorieten4 = 2130968606;
+			
+			// aapt resource value: 0x7f040025
+			public const int btnYoutubeInstellingen = 2130968613;
+			
+			// aapt resource value: 0x7f040033
+			public const int btnZiggo = 2130968627;
+			
+			// aapt resource value: 0x7f040011
+			public const int btnZiggoAccountVerwijderen = 2130968593;
+			
+			// aapt resource value: 0x7f040020
+			public const int btnZiggoFavorieten1 = 2130968608;
+			
+			// aapt resource value: 0x7f040021
+			public const int btnZiggoFavorieten2 = 2130968609;
+			
+			// aapt resource value: 0x7f040022
+			public const int btnZiggoFavorieten3 = 2130968610;
+			
+			// aapt resource value: 0x7f040023
+			public const int btnZiggoFavorieten4 = 2130968611;
+			
+			// aapt resource value: 0x7f040026
+			public const int btnZiggoInstellingen = 2130968614;
 			
 			// aapt resource value: 0x7f040005
 			public const int etPassword = 2130968581;
@@ -137,41 +164,41 @@ namespace Casus_Jaar_2_Blok_1_Groep_3
 			// aapt resource value: 0x7f040004
 			public const int etUsername = 2130968580;
 			
-			// aapt resource value: 0x7f040026
-			public const int linearLayout1 = 2130968614;
+			// aapt resource value: 0x7f04002f
+			public const int linearLayout1 = 2130968623;
 			
-			// aapt resource value: 0x7f04000f
-			public const int txtFavorieten = 2130968591;
-			
-			// aapt resource value: 0x7f040006
-			public const int txtNetflixACCV = 2130968582;
-			
-			// aapt resource value: 0x7f04000b
-			public const int txtNetflixAccountVerwijderen = 2130968587;
-			
-			// aapt resource value: 0x7f040010
-			public const int txtNetflixFavorieten = 2130968592;
-			
-			// aapt resource value: 0x7f040024
-			public const int txtOpenProgram = 2130968612;
-			
-			// aapt resource value: 0x7f040007
-			public const int txtYoutubeACCV = 2130968583;
-			
-			// aapt resource value: 0x7f040009
-			public const int txtYoutubeAccountVerwijderen = 2130968585;
-			
-			// aapt resource value: 0x7f040015
-			public const int txtYoutubeFavorieten = 2130968597;
+			// aapt resource value: 0x7f040012
+			public const int txtFavorieten = 2130968594;
 			
 			// aapt resource value: 0x7f040008
-			public const int txtZiggoACCV = 2130968584;
+			public const int txtNetflixAccV = 2130968584;
 			
-			// aapt resource value: 0x7f04000d
-			public const int txtZiggoAccountVerwijderen = 2130968589;
+			// aapt resource value: 0x7f04000e
+			public const int txtNetflixAccountVerwijderen = 2130968590;
+			
+			// aapt resource value: 0x7f040015
+			public const int txtNetflixFavorieten = 2130968597;
+			
+			// aapt resource value: 0x7f04002d
+			public const int txtOpenProgram = 2130968621;
+			
+			// aapt resource value: 0x7f040009
+			public const int txtYoutubeACCV = 2130968585;
+			
+			// aapt resource value: 0x7f04000c
+			public const int txtYoutubeAccountVerwijderen = 2130968588;
 			
 			// aapt resource value: 0x7f04001a
-			public const int txtZiggoFavorieten = 2130968602;
+			public const int txtYoutubeFavorieten = 2130968602;
+			
+			// aapt resource value: 0x7f04000a
+			public const int txtZiggoACCV = 2130968586;
+			
+			// aapt resource value: 0x7f040010
+			public const int txtZiggoAccountVerwijderen = 2130968592;
+			
+			// aapt resource value: 0x7f04001f
+			public const int txtZiggoFavorieten = 2130968607;
 			
 			static Id()
 			{
